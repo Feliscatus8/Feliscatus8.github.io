3d4f2ed5c7d51bbfb0db92dd26f4822fd8bc4fdd
Lech Ogórkiewicz <feliscatus@interia.eu>
2023-01-25 18:29:31 +0100

    changed api root

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://localhost:8080/v1/tasks';
+  var apiRoot = 'https://tasks-prod-kodilla-tasks-ga5c2x.mo2.mogenius.io/v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
